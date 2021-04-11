503a37fafa06b96def13f6ade865061dec90361d
Neamar <neamar@neamar.fr>
2021-04-11 18:54:21 +0200

    hmmm

--- cooperation/asgi.py
+++ cooperation/asgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cooperation.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import game.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cooperation.settings")
 
 application = ProtocolTypeRouter(
     {
