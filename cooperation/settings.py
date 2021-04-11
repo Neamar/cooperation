ac83a03833faef41768acae047eead6223d7e9c7
Neamar <neamar@neamar.fr>
2021-04-11 19:01:10 +0200

    added allowed host

--- cooperation/settings.py
+++ cooperation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9$awymr0!t5mp7sg^$$viq=6iod=g(6zrg)_2v)3!*f9h=c&7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cooperation.neamar.fr"]
 
 
 # Application definition
