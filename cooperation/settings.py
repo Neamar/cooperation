04d916c1ed8624143c29100d6d5c28f970a47e40
Neamar <neamar@neamar.fr>
2021-04-11 19:05:27 +0200

    use wss

--- cooperation/settings.py
+++ cooperation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9$awymr0!t5mp7sg^$$viq=6iod=g(6zrg)_2v)3!*f9h=c&7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cooperation.neamar.fr"]
+ALLOWED_HOSTS = ["127.0.0.1", "cooperation.neamar.fr"]
 
 
 # Application definition
--- rooms/players.js
+++
@@ -1,4 +0,0 @@
-export const randomPlayer = () => {};
-export const allPlayersExcept = (player) => {
-  return [];
-};
