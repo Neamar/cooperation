ba0e247e2096b798ae30736f9cff25fc5984d387
Neamar <neamar@neamar.fr>
2021-04-11 15:26:26 +0200

    Added model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class GameState(models.Model):
+    hash = models.IntegerField(unique=True)
+    created_at = models.DateField(auto_now_add=True)
+    game_state = models.JSONField()
+
+    def __str__(self):
+        print("%s") % hash
