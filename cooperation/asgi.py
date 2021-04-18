c3b367c9416089e326ff36df10e2715443ca4b70
Neamar <neamar@neamar.fr>
2021-04-18 22:52:34 +0200

    again

--- cooperation/asgi.py
+++ cooperation/asgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cooperation.settings")
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+django.setup()
 
 import game.routing
 
